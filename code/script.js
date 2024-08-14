d93e004264d9067130656837dcaf28d4dc747333
matthewbradley22 <mbradley22@utexas.edu>
2024-08-14 10:40:22 -0500

    Submit gets radio button value

--- code/script.js
+++ code/script.js
@@ -1,5 +1,6 @@
-const submit = document.getElementById("submit")
+const submit = document.getElementById("submit");
 
 submit.onclick = function(){
-    
+    let radioChoice = document.querySelector('input[name="math"]:checked').value;
+    console.log(radioChoice);
   }
